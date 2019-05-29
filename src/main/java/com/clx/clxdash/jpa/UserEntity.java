e1a409c7aea25a888aa5113a09d634bce81459ac
luhuichen <841105197@qq.com>
2019-05-30 00:31:20 +0800

    数据库创建

---
+++ src/main/java/com/clx/clxdash/jpa/UserEntity.java
@@ -0,0 +1,142 @@
+package com.clx.clxdash.jpa;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+@Table(name = "User", schema = "clxdash", catalog = "")
+public class UserEntity {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+    private Timestamp registertime;
+    private Timestamp lastlogintime;
+    private String level;
+    private String name;
+    private String address;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "username")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Basic
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Basic
+    @Column(name = "phone")
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Basic
+    @Column(name = "registertime")
+    public Timestamp getRegistertime() {
+        return registertime;
+    }
+
+    public void setRegistertime(Timestamp registertime) {
+        this.registertime = registertime;
+    }
+
+    @Basic
+    @Column(name = "lastlogintime")
+    public Timestamp getLastlogintime() {
+        return lastlogintime;
+    }
+
+    public void setLastlogintime(Timestamp lastlogintime) {
+        this.lastlogintime = lastlogintime;
+    }
+
+    @Basic
+    @Column(name = "level")
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "address")
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserEntity that = (UserEntity) o;
+        return id == that.id &&
+                Objects.equals(username, that.username) &&
+                Objects.equals(password, that.password) &&
+                Objects.equals(email, that.email) &&
+                Objects.equals(phone, that.phone) &&
+                Objects.equals(registertime, that.registertime) &&
+                Objects.equals(lastlogintime, that.lastlogintime) &&
+                Objects.equals(level, that.level) &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(address, that.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, email, phone, registertime, lastlogintime, level, name, address);
+    }
+}
