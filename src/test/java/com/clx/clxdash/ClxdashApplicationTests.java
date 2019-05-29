c0098aaf33059e98e9e0f48253e2f53d3df46752
陆慧晨 <luhuichen@luhuichendeMacBook-Pro.local>
2019-05-29 23:57:48 +0800

    Initial commit

---
+++ src/test/java/com/clx/clxdash/ClxdashApplicationTests.java
@@ -0,0 +1,16 @@
+package com.clx.clxdash;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClxdashApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
